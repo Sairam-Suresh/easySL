df40f36c1388076bc5bd1bbf3146e158f75f328f
Sairam-Suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-09-08 23:16:27 +0800

    Cleared Out some template code in the tabOne tsx

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+      <View>
+        
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // Template Styles
   container: {
     flex: 1,
     alignItems: 'center',
@@ -29,4 +27,6 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+
+  // Custom Styles
 });
